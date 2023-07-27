85a3236365f28b870c9614cb86d3fe9b7ab916d3
David Nguyen <h250694@gmail.com>
2023-07-27 09:29:25 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,13 +14,13 @@ cd docs/.vuepress/dist
 
 echo 'User-agent: *
 Disallow:
-Sitemap: https://devtut.github.io/sitemap.xml
-Host: https://devtut.github.io' > robots.txt
+Sitemap: https://code.pelus.dev/sitemap.xml
+Host: https://code.pelus.dev' > robots.txt
 
 git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:devtut/devtut.github.io.git master
+git push -f git@github.com:hoangsvit/hoangsvit.github.io.git master
 
 cd -
